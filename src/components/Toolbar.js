46c62f36ae2bcd7d1e6ebef0165b3a71b77fcaac
nlpulido <npulido@dons.usfca.edu>
2021-04-14 11:28:21 -0700

    setup links for the toolbar

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import { IconButton } from '@material-ui/core'
 import { withRouter } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
+
+import { IconButton } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
 import { faGithub, faLinkedinIn, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 import './Toolbar.css';
 
+function Toolbar({ isMobile, orientation }) {
+  const useMobileMenu = useMediaQuery({ query: '(max-width: 768px)' });
 
-function Toolbar(props) {
   return (
     <div className="Toolbar">
         <div className="header">
             <div className="title">
-              <h3>{">"} neil pulido<span class="blink">_</span></h3>
+              <button className="link" title="about" onClick={() => alert("going to home!")}>{">"} neil pulido<span class="blink">_</span></button>
             </div>
 
-            <div className="menu">
-              <h3>about</h3>
-              <h3>social</h3>
-              <h3>contact</h3>
-            </div>
+            {useMobileMenu ? (
+              <div className="mobileMenu">
+                <IconButton>
+                  <MenuIcon fontSize="medium" style={{ color: 'black' }} />
+                </IconButton>
+              </div>
+            ) : (
+              <div className="desktopMenu">
+                <button className="link" title="about" onClick={() => alert("going to about!")}>about</button>
+                <button className="link" title="social" onClick={() => alert("going to social!")}>social</button>
+                <button className="link" title="contact" onClick={() => alert("going to contact!")}>contact</button>
+              </div>
+            )}
         </div>
     </div>
   );
