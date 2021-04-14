aa9162a01ee85218b721ca1e46e4392ab9704d94
nlpulido <npulido@dons.usfca.edu>
2021-04-14 10:30:16 -0700

    working on creating a responsive webpage

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
       <Toolbar />
-      <Home />
-      <Footer />
+      {/* <Home />
+      <Footer /> */}
     </div>
   );
 }
--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -11,24 +11,15 @@ import './Toolbar.css';
 function Toolbar(props) {
   return (
     <div className="Toolbar">
-        <div className="title">
-            <h3>{">"} neil pulido<span class="blink">_</span></h3>
-            <div className="socials">
-              <IconButton href="https://www.github.com/nlpulido/">
-                <FontAwesomeIcon icon={faGithub} style={{ color: 'white' }}/>
-              </IconButton>
-              <IconButton href="https://www.linkedin.com/in/nlpulido/">
-                <FontAwesomeIcon icon={faLinkedinIn} style={{ color: 'white' }}/>
-              </IconButton>
-              <IconButton href="https://www.youtube.com/channel/UCN22en-BT8nzxksa5us0BGw/">
-                <FontAwesomeIcon icon={faYoutube} style={{ color: 'white' }}/>
-              </IconButton>
-              <IconButton href="https://www.instagram.com/neilisnext/">
-                <FontAwesomeIcon icon={faInstagram} style={{ color: 'white' }}/>
-              </IconButton>
-              <IconButton href="mailto:neilpphoto@gmail.com">
-                <FontAwesomeIcon icon={faEnvelope} style={{ color: 'white' }}/>
-              </IconButton>
+        <div className="header">
+            <div className="title">
+              <h3>{">"} neil pulido<span class="blink">_</span></h3>
+            </div>
+
+            <div className="menu">
+              <h3>about</h3>
+              <h3>social</h3>
+              <h3>contact</h3>
             </div>
         </div>
     </div>
