6d7448662898c6262b03d54af36194b9efe464df
nlpulido <npulido@dons.usfca.edu>
2021-04-16 15:14:37 -0700

    cleaned up Toolbar.js

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,13 +1,8 @@
 import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
-
 import { IconButton } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
-import { faGithub, faLinkedinIn, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 import './Toolbar.css';
 const links = [
@@ -27,9 +22,8 @@ const links = [
 
 function Toolbar() {
   const useMobileMenu = useMediaQuery({ query: '(max-width: 768px)' });
-  const [menuVisible, setMenuVisible] = useState(false); // TODO: Change this to false
+  const [menuVisible, setMenuVisible] = useState(false);
   const toggleMenu = () => setMenuVisible(!menuVisible);
-  const turnOffMenu = () => setMenuVisible(false);
 
   return (
     <div className="Toolbar">
