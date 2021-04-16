4f1a1976be1d2d0e9e2f03d351d83a27cecbc109
nlpulido <npulido@dons.usfca.edu>
2021-04-16 15:13:38 -0700

    rendered buttons per menu button

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -10,38 +10,71 @@ import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
 import { faGithub, faLinkedinIn, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 import './Toolbar.css';
+const links = [
+  {
+    title: "about",
+    alert: "going to about!"
+  },
+  {
+    title: "social",
+    alert: "going to social!"
+  },
+  {
+    title: "contact",
+    alert: "going to contact!"
+  }
+]
 
 function Toolbar() {
   const useMobileMenu = useMediaQuery({ query: '(max-width: 768px)' });
-  const [menuVisible, setMenuVisible] = useState(true); // TODO: Change this to false
+  const [menuVisible, setMenuVisible] = useState(false); // TODO: Change this to false
   const toggleMenu = () => setMenuVisible(!menuVisible);
+  const turnOffMenu = () => setMenuVisible(false);
 
   return (
     <div className="Toolbar">
         <div className="header">
             <div className="title">
-              <button className="link" title="about" onClick={() => alert("going to home!")}>{">"} neil pulido<span class="blink">_</span></button>
+              <button 
+                className="desktopLink"
+                title="about" onClick={() => alert("going to home!")}
+              >
+                {">"} neil pulido<span class="blink">_</span>
+              </button>
             </div>
 
             {useMobileMenu ? (
-              <div className="mobileMenu">
+              <div className="mobileMenuIcon">
                 <IconButton onClick={toggleMenu}>
                   <MenuIcon fontSize="medium" style={{ color: 'black' }} />
                 </IconButton>
               </div>
             ) : (
               <div className="desktopMenu">
-                <button className="link" title="about" onClick={() => alert("going to about!")}>about</button>
-                <button className="link" title="social" onClick={() => alert("going to social!")}>social</button>
-                <button className="link" title="contact" onClick={() => alert("going to contact!")}>contact</button>
+                {links.map((link, index) => 
+                  <button 
+                    className="desktopLink"
+                    key={index}
+                    onClick={() => alert(link.alert)}
+                  >
+                    {link.title}
+                  </button>
+                )}
               </div>
             )}
-
         </div>
 
-        {menuVisible ? (
-          <div>
-            <p>Menu Goes Here</p>
+        {menuVisible && useMobileMenu ? (
+          <div className="mobileMenu">
+            {links.map((link, index) => 
+              <button 
+                className="mobileLink"
+                key={index}
+                onClick={() => alert(link.alert)}
+              >
+                {link.title}
+              </button>
+            )}
           </div>
         ) : (
           <></>
