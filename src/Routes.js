8f9102f474cd27d06d27f10a405a8e2528f96049
Neil Pulido <neilpphoto@gmail.com>
2021-05-24 07:11:39 -0700

    added routes for the hobbies

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,9 @@ import { Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
+import keyboards from "./pages/Keyboards/keyboards";
+import photography from "./pages/Photography/photography";
+import fashion from "./pages/Fashion/fashion";
 
 /* History */
 import history from "./History";
@@ -16,6 +19,9 @@ function Routes() {
                 <Route path="/" exact component={Home} />
                 <Route path="/about" exact component={About} />
                 <Route path="/contact" exact component={Contact} />
+                <Route path="/keyboards" exact component={keyboards} />
+                <Route path="/photography" exact component={photography} />
+                <Route path="/fashion" exact component={fashion} />
             </Switch>
         </Router>
     );
