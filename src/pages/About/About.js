acfaf538ee2da3e4b0e0fa123ac68a00cbfccda1
Neil Pulido <neilpphoto@gmail.com>
2021-05-23 22:45:19 -0700

    working on the about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,8 +1,19 @@
 import { React } from "react";
+import graduation_headshot from "./graduation_headshot.jpg";
+import "./About.css";
 
 function About() {
     return(
-        <h1>Hi About</h1>
+        <div className="container">
+            <div className="contents">
+                <img src={graduation_headshot} alt={"Neil's Graduation Headshot"}></img>
+                <div className="description">
+                    <h4>Read About Me</h4>
+                    <h1>About Me</h1>
+                    <p>Hi there! My name's Neil & I just recently graduated from the University of San Francisco with a major in Computer Science.</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
