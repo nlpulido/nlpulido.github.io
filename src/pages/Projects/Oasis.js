9ba8119821a8732de59a841d4017ccea9633e276
Neil Pulido <neilpphoto@gmail.com>
2021-05-31 15:15:55 -0700

    finished description for Oasis Mobile

--- src/pages/Projects/Oasis.js
+++ src/pages/Projects/Oasis.js
@@ -1,12 +1,26 @@
 import { React } from "react";
+import iPhone from "./iphone_transparent.png";
 import "./Projects.css";
 
 function Oasis() {
     return (
         <div className="container">
             <div className="project-container">
-                <h4>Projects</h4>
-                <h1>Oasis</h1>
+                <img src={iPhone} alt={"iPhone Filler"} className="header_image"></img>
+                <div className="project-description">
+                    <h4>Projects</h4>
+                    <h1>Oasis Mobile</h1>
+                    <h3>Won “Best Team” / “Student Choice” Award</h3>
+                    <p>
+                        Oasis Mobile is a mobile application designed to engage users in 
+                        wellness practice. It allows users to pursue their journey towards 
+                        a healthier lifestyle through its menu of personalized mindfulness 
+                        exercises and programs. For the project, I led a team of CS students
+                        & business students in designing a UI with <span>Figma</span>, developing the front
+                        end using <span>React Native & Expo</span>, and creating the backend services using 
+                        <span> Express.JS, Node.JS, & Axiom.</span>
+                    </p>
+                </div>
             </div>
         </div>
     );
