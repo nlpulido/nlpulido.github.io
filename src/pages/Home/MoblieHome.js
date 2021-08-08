adec3b3e081ad61958c93673f8df8669e2e53f82
Neil Pulido <neilpphoto@gmail.com>
2021-08-07 21:38:43 -0700

    added a disclaimer to the mobile version

--- src/pages/Home/MoblieHome.js
+++ src/pages/Home/MoblieHome.js
@@ -4,12 +4,18 @@ import "./Home.scss";
 
 function MobileHome() {
     return (
-        <div className="portraitContainer">
-            <div className="eyeCatcherPortrait">
-                <h1>Hi there!</h1>
-                <h1>My name is Neil.</h1>
-                <h2>I'm a software engineer based in the Bay Area.</h2>
-                <h2>Let's make something great.</h2>
+        <div className="home-container">
+            <div className="intro-container">
+                <div className="disclaimer">
+                    <p>The mobile version of this site is currently a work in progress! 🌱</p>
+                </div>
+                <div className="desktop-intro">
+                    <h1>Hi there!</h1>
+                    <h1>My name is Neil.</h1>
+                    <h2>I'm a software engineer based in the Bay Area.</h2>
+                    <h2>Let's make something great.</h2>
+                </div>
+                <img src={headshot} alt="headshot" id="headshot"></img>
             </div>
         </div>
     );
